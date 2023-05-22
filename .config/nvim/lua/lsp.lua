369ff523a6f3a26bbd9a18d6b7ed24d9257787e3
pigeonhands <sam.mackenzie@live.com>
2023-05-22 20:36:12 +1200

    gd

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,4 +1,7 @@
 -- Completion Plugin Setup
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
 local cmp = require'cmp'
 cmp.setup({
   -- Enable LSP snippets
--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -9,6 +9,10 @@ vim.opt.wrap = false
 vim.opt.nu = true
 vim.opt.termguicolors = true
 
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+
 -- for nerdtree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
