1d6d1e73a1b5767286bc196c1d036f454577d898
pigeonhands <git@s-mc.io>
2023-06-07 21:28:05 +1200

    remove lsp print

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -56,7 +56,6 @@ cmp.setup({
 
 require("mason-lspconfig").setup_handlers {
 	function (server_name) -- default handler (optional)
-		print(server_name)
 		require("lspconfig")[server_name].setup {}
 	end,
 	["rust_analyzer"] = function ()
