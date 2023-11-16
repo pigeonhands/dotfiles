03443e8ca4603680c7c250c924a45f836f689d71
pigeonhands <git@s-mc.io>
2023-11-16 17:21:48 +1300

    add diagnostic keysi, extra grep keys and git keys

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -6,19 +6,38 @@ vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap
 vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',  {  noremap = true, silent = true })
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>',  {  noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fm', '<cmd>lua vim.lsp.buf.format()<CR>',  {  noremap = true, silent = true })
+vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", {noremap = true})
+
+-- Diagnostics
+vim.keymap.set('n', '<leader>ge', '<cmd>:Trouble<CR>', {  noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- CodeLens
+vim.keymap.set('n', '<leader>clld', vim.lsp.codelens.refresh)
+vim.keymap.set('n', '<leader>clr', vim.lsp.codelens.run)
+
 
 local telescope = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', telescope.git_files, {})
 vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
+vim.keymap.set('n', '<leader>ft', telescope.grep_string, {})
 vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
 vim.keymap.set('n', '<leader>fr', telescope.lsp_references, {})
+vim.keymap.set('n', '<leader>fs', telescope.treesitter, {})
 
-vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>",
-                        {noremap = true})
-vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>",
-                        {noremap = true})
+-- Git
+vim.keymap.set("n", "ggc", telescope.git_commits)
+vim.keymap.set("n", "ggs", telescope.git_status)
+
+
+
+
+vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>",{noremap = true})
 -- vim.keymap.set("n", "<leader>fm", "<cmd>Neoformat<CR>" {noremap = true})
 
 vim.keymap.set('n', '<leader>`', '<cmd>ToggleTerm<CR>', { noremap = true })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,46 +1,47 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  'wbthomason/packer.nvim',
-    -- > theme
-  {'rose-pine/neovim', as = 'rose-pine'},
+	'wbthomason/packer.nvim',
+	-- > theme
+	{ 'rose-pine/neovim',           as = 'rose-pine' },
 	{ 'projekt0n/github-nvim-theme' },
 
-    -- > fucntionality
+	-- > fucntionality
 	'lewis6991/gitsigns.nvim',
 	'nvim-tree/nvim-tree.lua',
 	'nvim-tree/nvim-web-devicons',
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        "folke/trouble.nvim",
-        dependencies = "nvim-tree/nvim-web-devicons",
-        config = function()
-            require("trouble").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    },
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.1',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("trouble").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end
+	},
 	'windwp/nvim-autopairs',
 	"akinsho/toggleterm.nvim",
 
-	--> ui 
-  {
+	--> ui
+	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
 	},
@@ -49,33 +50,33 @@ require('lazy').setup({
 	'folke/which-key.nvim',
 	'xiyaowong/transparent.nvim',
 
-    -- > code / LSP
+	-- > code / LSP
 
-    -- 'sbdchd/neoformat',
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    {'nvim-treesitter/nvim-treesitter', run = {":TSUpdate"}},
-	{'nvim-treesitter/nvim-treesitter-context'},
+	-- 'sbdchd/neoformat',
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	{ 'nvim-treesitter/nvim-treesitter',        run = { ":TSUpdate" } },
+	{ 'nvim-treesitter/nvim-treesitter-context' },
 
-    -- Completion framework:
-    'hrsh7th/nvim-cmp',
+	-- Completion framework:
+	'hrsh7th/nvim-cmp',
 
-    -- LSP completion source:
-    'hrsh7th/cmp-nvim-lsp',
+	-- LSP completion source:
+	'hrsh7th/cmp-nvim-lsp',
 
-    -- Useful completion sources:
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/vim-vsnip',
+	-- Useful completion sources:
+	'hrsh7th/cmp-nvim-lua',
+	'hrsh7th/cmp-nvim-lsp-signature-help',
+	'hrsh7th/cmp-vsnip',
+	'hrsh7th/cmp-path',
+	'hrsh7th/cmp-buffer',
+	'hrsh7th/vim-vsnip',
 
-    'simrat39/rust-tools.nvim',
+	'simrat39/rust-tools.nvim',
 
-	  "rafamadriz/friendly-snippets",
-    'nvim-telescope/telescope-ui-select.nvim',
+	"rafamadriz/friendly-snippets",
+	'nvim-telescope/telescope-ui-select.nvim',
 })
 
 require("mason").setup()
@@ -84,11 +85,11 @@ require("mason-lspconfig").setup()
 require('gitsigns').setup {
 	current_line_blame = true,
 	current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 500,
-    ignore_whitespace = false,
-  },
+		virt_text = true,
+		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+		delay = 500,
+		ignore_whitespace = false,
+	},
 }
 
 
@@ -97,24 +98,24 @@ require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort_by = "case_sensitive",
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
-require'treesitter-context'.setup{
+require 'treesitter-context'.setup {
 }
 
 require('lualine').setup()
 require('nvim-autopairs').setup()
 
-require'alpha'.setup(require'alpha.themes.dashboard'.config)
+require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
 require("which-key").setup()
 vim.o.timeout = true
 vim.o.timeoutlen = 500
