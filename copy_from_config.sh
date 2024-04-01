310f1bccab3d64abab009b4ffc536eaf1d2a2ba6
Sam M <git@s-mc.io>
2024-04-02 12:52:30 +1300

    added zsh plugins

--- copy_from_config.sh
+++ copy_from_config.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-cp -r ~/.zshrc ./.zshrc
-cp -r ~/.config/zsh/* ./.config/zsh
-cp -r ~/.config/alacritty/* ./.config/alacritty/
-cp -r ~/.config/nvim/* ./.config/nvim/
+function copy_file() {
+	cp ~/$1 ./$1
+}
+
+function copy_dir() {
+	cp -r ~/$1/* ./$1/
+}
+
+copy_file ".zshrc"
+copy_file ".zsh_plugins.txt"
+copy_dir ".config/zsh"
+copy_dir ".config/alacritty"
+copy_dir ".config/nvim"
+#cp -r ~/.zshrc ./.zshrc
+#cp -r ~/.zsh_plugins.txt ./.zsh_plugins.txt
+#cp -r ~/.config/zsh/* ./.config/zsh
+#cp -r ~/.config/alacritty/* ./.config/alacritty/
+#cp -r ~/.config/nvim/* ./.config/nvim/
