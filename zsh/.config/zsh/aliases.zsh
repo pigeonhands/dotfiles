784c606f64c43d731ba6a73e5e9cb231d1c06386
Sam M <sam.mackenzie@sharesies.co.nz>
2025-07-10 12:22:05 +1200

    make zsh alias more robust

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,10 +1,18 @@
 
-alias grep='rg'
-alias find=fd
+
 #alias cat='batcat --style=plain'
-alias cat=bat
-alias l='eza'
-alias la='eza -a'
-alias ll='eza -lah'
-alias ls='eza --color=auto'
+
+command -v rg >/dev/null  && alias grep='rg'
+# command fd && alias find='fd'
+
+command -v batcat >/dev/null && alias cat='batcat'
+command -v bat >/dev/null && alias cat='bat'
+
+
+if command -v eza >/dev/null; then
+    alias l='eza'
+    alias la='eza -a'
+    alias ll='eza -lah'
+    alias ls='eza --color=auto'
+fi
 
