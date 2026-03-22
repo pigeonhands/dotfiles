b22d01c25ec19b9b16a1d0c609a20fd2de3edc5d
Sam M <git@s-mc.io>
2026-03-22 14:56:48 +1300

    Set ghostty theme and set wagon ssh key signing

--- common/zsh/dot-config/zsh/init.zsh
+++ common/zsh/dot-config/zsh/init.zsh
@@ -42,6 +42,23 @@ elif [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]; then
     source /usr/share/doc/fzf/examples/completion.zsh
 fi
 
+export SSH_ENV="$XDG_RUNTIME_DIR/ssh-agent.env"
+
+function start_agent {
+    /usr/bin/ssh-agent -t 1h >| "${SSH_ENV}"
+    chmod 600 "${SSH_ENV}"
+    source "${SSH_ENV}" > /dev/null
+}
+
+if [ -f "${SSH_ENV}" ]; then
+    source "${SSH_ENV}" > /dev/null
+    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent > /dev/null || {
+        start_agent;
+    }
+else
+    start_agent;
+fi
+
 keys_file="$HOME/.config/ai/keys.sh"
 
 if [[ -f "$keys_file" ]]; then
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,6 @@
 # run with ./setup.sh sync
 
 sync_dotfiles "common"
+
 hostname="$(hostname -s)"
-[[ -d "$hostname" ]] && sync_dotfiles "$hostname" || true
+[[ -d "./machines/$hostname" ]] && sync_dotfiles "./machines/$hostname" || true
